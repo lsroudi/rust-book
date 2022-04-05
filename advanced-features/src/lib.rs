53a29ad459e01b0efb612f9c39ae4b3ac296367c
Abdessamad el akroudi <lsroudi@gmail.com>
2022-04-05 16:03:39 +0200

    add function pointer

--- advanced-features/src/lib.rs
+++ advanced-features/src/lib.rs
@@ -93,3 +93,20 @@ pub fn demo_super_trait(){
     let p = Point{x:3,y:6};
     p.outline_print();
 }
+
+// function pointer
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
+
+pub fn demo_function_pointer() {
+    let answer = do_twice(add_one, 5);
+
+    println!("The answer is: {}", answer);
+}
+
+
--- advanced-features/src/main.rs
+++ advanced-features/src/main.rs
@@ -2,4 +2,5 @@ fn main() {
     //advanced_features::demo_raw_pointers();
     advanced_features::demo_trait_associated_type();
     advanced_features::demo_super_trait();
+    advanced_features::demo_function_pointer();
 }
